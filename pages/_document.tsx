1d4222af89486e8db7af49cbddb4b938f81fd387
Filip <filipmarkovic@gmail.com>
2023-02-13 16:35:24 +0100

    added sitemap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 import Script from "next/script"
 
 export default function Document() {
+
     return (
         <Html lang="hr-HR">
             <Head>
@@ -19,10 +20,11 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
                 <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} strategy="afterInteractive" />
-                <Script id="ga-script" strategy="afterInteractive">
+                <Script id="gtag" strategy="afterInteractive">
                     {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
+
                     gtag('js', new Date());
                     gtag('config', '${process.env.GOOGLE_ANALYTICS}');
                     `}
